932796b319c42bde637fb80850376d38e5c75821
vinston12 <dom.boberek@gmail.com>
2019-05-29 22:55:23 +0200

    register update

--- app/src/main/java/com/example/snapchatcopy/ActivitySingIn.java
+++ app/src/main/java/com/example/snapchatcopy/ActivitySingIn.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -26,10 +27,14 @@ import java.util.Map;
 
 public class ActivitySingIn extends AppCompatActivity {
 
+    EditText nameLabel, passLabel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.singin_main);
+
+
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
--- app/src/main/java/com/example/snapchatcopy/ActivitySingUp.java
+++ app/src/main/java/com/example/snapchatcopy/ActivitySingUp.java
@@ -1,8 +1,75 @@
 package com.example.snapchatcopy;
 
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.widget.EditText;
 
-public class ActivitySingUp  {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.core.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActivitySingUp extends AppCompatActivity {
+
+    EditText emailLabel, nameLabel, passLabel;
+    CardView bLogin;
+    String email, name, pass;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.singup_main);
+
+
+        bLogin = (CardView)findViewById(R.id.loginButtonIn);
+
+        emailLabel = (EditText)findViewById(R.id.emailLabel);
+        nameLabel  = (EditText)findViewById(R.id.nameLabel);
+        passLabel = (EditText)findViewById(R.id.passLabel);
+
+
+        bLogin.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+
+                email = emailLabel.getText().toString();
+                name = nameLabel.getText().toString();
+                pass = passLabel.getText().toString();
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("Email", email);
+                user.put("Name", name);
+                user.put("Pass", pass);
+
+                // Add a new document with a generated ID
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("User", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("User", "Error adding document", e);
+                            }
+                        });
+            }
+        });
+
+    }
 
 
 }
