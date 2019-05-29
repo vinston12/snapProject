10b2e52539aae59eb991c09c5b9627be23bb44f8
vinston12 <dom.boberek@gmail.com>
2019-05-29 21:52:27 +0200

    update firebase

--- app/src/main/java/com/example/snapchatcopy/ActivitySingIn.java
+++ app/src/main/java/com/example/snapchatcopy/ActivitySingIn.java
@@ -1,11 +1,19 @@
 package com.example.snapchatcopy;
 
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 
 public class ActivitySingIn extends AppCompatActivity {
 
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.singin_main);
+        Toast.makeText(this, "Firebase podpiety!!", Toast.LENGTH_LONG).show();
+    }
 }
