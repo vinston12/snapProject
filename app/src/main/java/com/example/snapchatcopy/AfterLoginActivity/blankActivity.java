1368c259311782c09f2d78cdf40c6fa10f0fce65
vinston12 <dom.boberek@gmail.com>
2019-06-23 01:01:49 +0200

    dodanie slide fragmentow

---
+++ app/src/main/java/com/example/snapchatcopy/AfterLoginActivity/blankActivity.java
@@ -0,0 +1,13 @@
+package com.example.snapchatcopy.AfterLoginActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class blankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank);
+    }
+}
